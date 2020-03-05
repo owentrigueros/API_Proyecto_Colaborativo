b9417e98f11a28a73e3f201123b5da3bd27d095f
oihanealbizuri <oalbizuri001@ikasle.ehu.eus>
2020-03-05 19:20:51 +0100

    Localizar tweets en mapa

--- tweetloc/js/functions.js
+++ tweetloc/js/functions.js
@@ -1,9 +1,9 @@
 function refreshMapCoordinates(lat, lon) {
     var map = document.getElementById("1");
 
-    var new_map_src_1 = "https://maps.google.com/maps?width=100%&height=600&hl=en";
-    var new_map_src_2 = "&q=" + lat + ',' + lon;
-    var new_map_src_3 = "&ie=UTF8&t=&z=14&iwloc=B&output=embed";
+    var new_map_src_1 = "https://www.openstreetmap.org/";
+    var new_map_src_2 = "?mlat=" + lat + "&amp;mlon=" + lon;
+    var new_map_src_3 = "#map=13/" + lat + "/" + lon;
 
     var new_map_src = new_map_src_1 + new_map_src_2 + new_map_src_3;
 
@@ -13,11 +13,7 @@ function refreshMapCoordinates(lat, lon) {
 function refreshMapPlace(place) {
     var map = document.getElementById("1");
 
-    var new_map_src_1 = "https://maps.google.com/maps?width=100%&height=600&hl=en";
-    var new_map_src_2 = "&q=" + place;
-    var new_map_src_3 = "&ie=UTF8&t=&z=14&iwloc=B&output=embed";
-
-    var new_map_src = new_map_src_1 + new_map_src_2 + new_map_src_3;
+    var new_map_src = "https://nominatim.openstreetmap.org/search?q=" + place;
 
     document.getElementById("1").setAttribute("src", new_map_src)
 }
